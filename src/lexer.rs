18d7bbbba1e8730fa5193cf16858ca2a88eb2745
Sami Timsina <stimsina@protonmail.com>
2021-04-29 13:40:03 -0500

    Lexer struct, started parser

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[derive(Debug)]
 pub enum Token {
     Number(f64),
@@ -24,84 +26,101 @@ pub enum Token {
     Caret,
     
     Semicolon,
+
+    UNDEFINED,
 }
 
-pub fn tokenize(input: String) -> Vec<Token> {
-    let mut tokens = Vec::new();
-    let mut iter = input.chars().peekable();
+#[derive(Debug)]
+pub struct Lexer {
+    tokens: VecDeque<Token>
+}
 
-    while let Some(c) = iter.next() {
-        match c {
-            '0'..='9' => {
-                let mut buffer = String::new();
-                buffer += &c.to_string();
-                while let Some(d) = iter.peek() {
-                    match d {
-                        '0'..='9' => buffer += &iter.next().unwrap().to_string(),
-                        _ => break,
-                    }
-                }
-                tokens.push(Token::Number(buffer.parse::<f64>().expect("Failed to parge to f64")));
-            },
-            x if x.is_alphabetic() => {
-                let mut buffer = String::new();
-                buffer += &c.to_string();
-                while let Some(d) = iter.peek() {
-                    match d {
-                        y if y.is_alphabetic() => buffer += &iter.next().unwrap().to_string(),
-                        _ => break,
-                    }
-                }
-                match buffer.as_str() {
-                    "true" => tokens.push(Token::Bool(true)),
-                    "false" => tokens.push(Token::Bool(false)),
-                    _ => tokens.push(Token::Identifier(buffer)),
-                }
-            },
-            '(' => tokens.push(Token::LeftParen),
-            ')' => tokens.push(Token::RightParen),
-            '=' => tokens.push(
-                if *iter.peek().unwrap_or(&'\0') == '=' {
-                    iter.next();
-                    Token::EqualEqual
-                } else {
-                    Token::Equal
-                }
-            ),
-            '!' => tokens.push(
-                if *iter.peek().unwrap_or(&'\0') == '=' {
-                    iter.next();
-                    Token::NotEqual
-                } else {
-                    Token::Not
-                }
-            ),
-            '>' => tokens.push(
-                if *iter.peek().unwrap_or(&'\0') == '=' {
-                    iter.next();
-                    Token::GreaterEqual
-                } else {
-                    Token::Greater
-                }
-            ),
-            '<' => tokens.push(
-                if *iter.peek().unwrap_or(&'\0') == '=' {
-                    iter.next();
-                    Token::LesserEqual
-                } else {
-                    Token::Lesser
-                }
-            ),
-            '+' => tokens.push(Token::Plus),
-            '-' => tokens.push(Token::Dash),
-            '*' => tokens.push(Token::Asterisk),
-            '/' => tokens.push(Token::Slash),
-            '%' => tokens.push(Token::Percent),
-            '^' => tokens.push(Token::Caret),
-            ';' => tokens.push(Token::Semicolon),
-            _ => (),
-        }
+impl Lexer {
+    pub fn next(&mut self) -> Token {
+        self.tokens.pop_back().unwrap_or(Token::UNDEFINED)
     }
 
-    return tokens;
+    pub fn peek(&self) -> &Token {
+        self.tokens.back().unwrap_or(&Token::UNDEFINED)
+    }
+
+    pub fn new(input: String) -> Lexer {
+        let mut tokens = VecDeque::new();
+        let mut iter = input.chars().peekable();
+
+        while let Some(c) = iter.next() {
+            match c {
+                '0'..='9' => {
+                    let mut buffer = String::new();
+                    buffer += &c.to_string();
+                    while let Some(d) = iter.peek() {
+                        match d {
+                            '0'..='9' => buffer += &iter.next().unwrap().to_string(),
+                            _ => break,
+                        }
+                    }
+                    tokens.push_back(Token::Number(buffer.parse::<f64>().expect("Failed to parge to f64")));
+                },
+                x if x.is_alphabetic() => {
+                    let mut buffer = String::new();
+                    buffer += &c.to_string();
+                    while let Some(d) = iter.peek() {
+                        match d {
+                            y if y.is_alphabetic() => buffer += &iter.next().unwrap().to_string(),
+                            _ => break,
+                        }
+                    }
+                    match buffer.as_str() {
+                        "true" => tokens.push_back(Token::Bool(true)),
+                        "false" => tokens.push_back(Token::Bool(false)),
+                        _ => tokens.push_back(Token::Identifier(buffer)),
+                    }
+                },
+                '(' => tokens.push_back(Token::LeftParen),
+                ')' => tokens.push_back(Token::RightParen),
+                '=' => tokens.push_back(
+                    if *iter.peek().unwrap_or(&'\0') == '=' {
+                        iter.next();
+                        Token::EqualEqual
+                    } else {
+                        Token::Equal
+                    }
+                ),
+                '!' => tokens.push_back(
+                    if *iter.peek().unwrap_or(&'\0') == '=' {
+                        iter.next();
+                        Token::NotEqual
+                    } else {
+                        Token::Not
+                    }
+                ),
+                '>' => tokens.push_back(
+                    if *iter.peek().unwrap_or(&'\0') == '=' {
+                        iter.next();
+                        Token::GreaterEqual
+                    } else {
+                        Token::Greater
+                    }
+                ),
+                '<' => tokens.push_back(
+                    if *iter.peek().unwrap_or(&'\0') == '=' {
+                        iter.next();
+                        Token::LesserEqual
+                    } else {
+                        Token::Lesser
+                    }
+                ),
+                '+' => tokens.push_back(Token::Plus),
+                '-' => tokens.push_back(Token::Dash),
+                '*' => tokens.push_back(Token::Asterisk),
+                '/' => tokens.push_back(Token::Slash),
+                '%' => tokens.push_back(Token::Percent),
+                '^' => tokens.push_back(Token::Caret),
+                ';' => tokens.push_back(Token::Semicolon),
+                _ => (),
+            }
+        }
+
+        Lexer { tokens }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod lexer;
+mod parser;
 
 use lexer::*;
+use parser::*;
 
 fn main() {
     loop {
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        println!("{:?}", tokenize(input));
+
+        let lexer = Lexer::new(input);
+        println!("{:?}", lexer);
     }
 }
